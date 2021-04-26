be1914058ac6622625daf9b045dd5e86d1211b22
andreippsq <82779322+andreippsq@users.noreply.github.com>
2021-04-26 08:45:33 +0300

    Add files via upload

---
+++ controllers/RegistrationController.java
@@ -0,0 +1,33 @@
+package com.example.tutorial.controllers;
+
+import com.example.tutorial.other.User;
+import com.example.tutorial.services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.sql.SQLException;
+
+@Controller
+public class RegistrationController {
+    private final UserService userService = new UserService();
+
+    @GetMapping(value = "/register")
+    public String showForm(Model model){
+        User user = new User();
+        model.addAttribute("user", user);
+        return "register_form";
+    }
+
+    @PostMapping(value = "/register")
+    public String submitForm(@ModelAttribute("user") User user) throws SQLException, ClassNotFoundException {
+        if(userService.userExists(user.getUsername())){
+            System.out.println("found");
+        }
+        else userService.addUser(user.getName(), user.getUsername(), user.getEmail(), user.getPassword(), user.getAddress(), user.getTelephone());
+
+        return "register_success";
+    }
+}
